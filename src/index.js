a35aaf2f7099dffaa752ca049d35a4fb6bf37cf8
Sakh-balti <f23bdats1e01005@iub.edu.pk>
2026-08-26 19:31:59 +0500

    define the routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-// import mongoose from "mongoose";
-// import { DB_NAME } from "./constants";
-// import express from "express";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 
 dotenv.config({
-  path: "./env",
+  path: "./.env",
 });
 connectDB()
   .then(() => {
@@ -16,18 +14,3 @@ connectDB()
   .catch((err) => {
     console.log("Mongodb connection failed !!! ", err);
   });
-// const app = express()(async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//     app.on("error", (error) => {
-//       console.log("ERROR", error);
-//       throw error;
-//     });
-//     app.listen(process.env.PORT, () => {
-//       console.log(`app is listening on port ${process.env.PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("Error: ", error);
-//     throw error;
-//   }
-// })();
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (LocalFilePath) => {
+  try {
+    if (!LocalFilePath) return null;
+    const response = await cloudinary.uploader.upload(LocalFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File is uploaded on cloudinary", response.url());
+    return response;
+  } catch (error) {
+    fs.unlinkSync(LocalFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
